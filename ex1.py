6e185b1c5f352aecd81957e5fa841ce829b486ba
lalith-ly <lalithyadhav2007@gmail.com>
2025-07-31 13:56:34 +0530

    program 1

---
+++ ex1.py
@@ -0,0 +1,15 @@
+print("ARITHEMATIC OPREATIONS")
+print("----------------------")
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("result:")
+c=a+b
+print("add:",c)
+c=a-b
+print("sub:",c)
+c=a*b
+print("mul:",c)
+c=a/b
+print("div:",c)
+c=a%b
+print("mod:",c)
